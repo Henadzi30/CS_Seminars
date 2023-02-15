af43d5035f745935242f4d9917a005ea05fd08c7
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-15 21:20:14 +0300

    added method for determining the multiplicity of the input number to the numbers 7 and 23

---
+++ CS_Seminar_14.02.2023_14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.WriteLine("Введите число а: ");
+int digit_a = Convert.ToInt32(Console.ReadLine());
+
+if (digit_a % 7 == 0 & digit_a % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
