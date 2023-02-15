380b938d5b43cc8909e63b5dc6b380927636a253
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-15 21:38:40 +0300

    added method to determine if one number is the square of another

---
+++ CS_Seminar_14.02.2023_16/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8, 9  ->  нет
+
+Console.WriteLine("Введите число а: ");
+int digit_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int digit_b = Convert.ToInt32(Console.ReadLine());
+
+
+if (digit_a * digit_a == digit_b | digit_b * digit_b == digit_a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
