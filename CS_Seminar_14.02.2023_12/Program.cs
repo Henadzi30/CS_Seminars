c849fe4eb3c0ff3cf2407c3677601c765be4e04f
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-15 20:53:52 +0300

    added method for determining the multiplicity of two input numbers

---
+++ CS_Seminar_14.02.2023_12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+//  является ли второе число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+
+Console.WriteLine("Введите число а: ");
+int digit_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int digit_b = Convert.ToInt32(Console.ReadLine());
+
+int result = digit_a % digit_b;
+if (result != 0)
+{
+    Console.WriteLine($"Не кратно, остаток от деления: {result}");
+}
+else
+{
+    Console.WriteLine($"Число {digit_a} кратно числу {digit_b}");
+} 
+
+
