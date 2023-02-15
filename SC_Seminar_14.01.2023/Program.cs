e6b2ee8a5943be8d0f52c01600bf40cafeaeceb3
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-15 17:25:17 +0300

    added a method for determining the largest number in a two-digit digit whith print result in oneline

---
+++ SC_Seminar_14.01.2023/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, 
+
+int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 10;
+if (firstDigit > lastDigit)
+{
+    // Console.WriteLine("max");
+    // Console.WriteLine(firstDigit);
+    // Console.WriteLine("min");
+    // Console.WriteLine(lastDigit);       
+    Console.WriteLine($"max {firstDigit}, min {lastDigit}");
+}
+else
+{
+    // Console.WriteLine("max");
+    // Console.WriteLine(lastDigit);
+    // Console.WriteLine("min");
+    // Console.WriteLine(firstDigit);           
+    Console.WriteLine($"max {lastDigit}, min {firstDigit}");
+}
