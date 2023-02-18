0e9a087877d8a834da63634c1561d09c0808921b
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-18 21:09:39 +0300

    modified a method

--- CS_Seminar_17.02.2023_21/Program.cs
+++ CS_Seminar_17.02.2023_21/Program.cs
@@ -7,11 +7,15 @@
 // AB = √(xb - xa)2 + (yb - ya)2
 
 Console.WriteLine("Введите координаты первой точки 'А': ");
+Console.WriteLine("Значение точки 'А' по оси 'x': ");
 double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Значение точки 'A' по оси 'y': ");
 double yA = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Введите координаты первой точки 'B': ");
+Console.WriteLine("Значение точки 'B' по оси 'x': ");
 double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Значение точки 'B' по оси 'y': ");
 double yB = Convert.ToDouble(Console.ReadLine());
 
 double distance = (Math.Sqrt(Math.Pow(xB - xA , 2) + Math.Pow(yB - yA, 2)));
