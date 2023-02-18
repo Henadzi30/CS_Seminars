539572b77566c8a6429c7c4452ac2bb0d559cec2
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-18 20:53:02 +0300

    added a method for determining the distance between points on the coordinate plane

---
+++ CS_Seminar_17.02.2023_21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// AB = √(xb - xa)2 + (yb - ya)2
+
+Console.WriteLine("Введите координаты первой точки 'А': ");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки 'B': ");
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+
+double distance = (Math.Sqrt(Math.Pow(xB - xA , 2) + Math.Pow(yB - yA, 2)));
+
+Console.WriteLine(Math.Round(distance, 2));
+
+
