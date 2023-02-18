9b190e200604ca85eb4b71228c5a5fef3f91e3e0
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-18 15:50:23 +0300

    added a method for determining the coordinate area by the number of the coordinate quarter

---
+++ CS_Seminar_17.02.2023_18/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// 1 -> “x > 0 && y > 0” 
+// 2 -> “x < 0 && y > 0” 
+// 3 -> “x < 0 && y < 0” 
+// 4 -> “x > 0 && y < 0”
+// 5 -> “Неверно введен номер четверти” 
+
+
+Console.WriteLine("Введите номер четверти, чтобы узнать диапазон координат: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+}
+else if (quarter == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if (quarter == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if (quarter == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+else
+{
+    Console.WriteLine("Неверно введен номер четверти");
+}
