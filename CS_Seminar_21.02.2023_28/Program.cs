c9a8f8742873803b343a2c6254c461cc57587114
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-22 17:06:51 +0300

    added a method for determining the product of numbers from 1 to N

---
+++ CS_Seminar_21.02.2023_28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+// 4 -> 24 
+// 5 -> 120
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int productOfNumber(int n)
+{
+    int product = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        product *= i;
+    }
+    return product;
+}
+
+int number = ReadNumber("Введите число N");
+int result = productOfNumber(number);
+Console.WriteLine(result);
