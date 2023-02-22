7e24563f33dddc41955a1a8a3c36a22a5b8eb5b2
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-22 17:03:07 +0300

    added a method for determining the number of digits in a number

---
+++ CS_Seminar_21.02.2023_26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int countDigitN(int n)
+{
+    int count = 0;
+    while (n != 0)
+    {
+        n /= 10;
+        count += 1;
+    }
+    return count;
+}
+
+int number = ReadNumber("Введите число: ");
+int result = countDigitN(number);
+Console.WriteLine(result);
