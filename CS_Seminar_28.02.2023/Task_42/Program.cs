cb7e7e5834ebb95465b6d4c2656e70538d53af06
Henadzi_Misuna <henadzi30@gmail.com>
2023-03-01 16:48:21 +0300

    added decimal to binary conversion method

--- CS_Seminar_28.02.2023/Task_42/Program.cs
+++ CS_Seminar_28.02.2023/Task_42/Program.cs
@@ -4,16 +4,31 @@
 // 3  -> 11
 // 2  -> 10
 
-int n = 44;
+
+// Задаем метод запроса информации от пользователя
+int ReadNumber(string massageToUser)
+{
+    Console.WriteLine(massageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+// Блок запрашиваемoй у пользователя информации
+int numberFromUser = ReadNumber("Введите десятичное число для преобразования в двоичное: ");
+
+// Блок определения длины двоичного числа
+int n = numberFromUser;
 int countOfDigits = 0;
-int nCopy = n;
+int nCopy = numberFromUser;
 
 while (nCopy != 0)
 {
     nCopy /= 2;
     countOfDigits++;
 }
-Console.WriteLine(countOfDigits);
+Console.WriteLine($"Длина массива десятичного числа {numberFromUser} в двоичном коде: {countOfDigits}");
+
+// Метод перевода десятичного числа в перевернутое двоичное число
 
 int[] binaryNumber = new int[countOfDigits];
 
@@ -23,15 +38,20 @@ for (int i = 0; i < binaryNumber.Length; i++)
     n /= 2;
 } 
 
-Console.WriteLine(string.Join("", binaryNumber));
-ReverseArray(binaryNumber);
-Console.WriteLine(string.Join("", binaryNumber));
-
+// Задаем метод замены элементов массива (первые с последними) - преобразование в двочный код 
 void ReversArray(int[] array)
 {
     for (int i = 0; i < array.Length / 2; i++)
     {
         int temp = array[i];
-        array[i] 
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
     }
 }
+
+
+Console.WriteLine($"Рузультат деления десятичного числа на '2': {string.Join("", binaryNumber)}");
+ReversArray(binaryNumber);
+Console.WriteLine($"Результат преобразования десятичного числа {numberFromUser} в двоичное: {string.Join("", binaryNumber)}");
+
+
