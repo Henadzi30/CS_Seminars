db58f5ca83175ca5cabfa87797c864bc499a5e01
Henadzi_Misuna <henadzi30@gmail.com>
2023-03-01 17:30:16 +0300

    added a method for converting a number to the Fibonacci sequence

--- CS_Seminar_28.02.2023/Task_44/Program.cs
+++ CS_Seminar_28.02.2023/Task_44/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
 
 int ReadNumber(string massageToUser)
 {
@@ -7,14 +12,21 @@ int ReadNumber(string massageToUser)
     return value;
 }
 
-int Fibonachi(int n)
+
+
+int[] Fibonachi(int n)
 {
-    if (n == 0 || n == 1) return n;
-     
-    return Fibonachi(n - 1) + Fibonachi(n - 2);
+    int[] array = new int[n];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 2] + array[i - 1];
+    }
+    return array;
 }
  
 int fib4 = ReadNumber("Введите число для расчета чисел Фибоначчи: ");
-int fib5 = Fibonachi(fib4);
+int[] fib5 = Fibonachi(fib4);
  
-Console.WriteLine(string.Join(", ", fib5));
+Console.WriteLine($"Последовательность чисел Фибоначчи от заданного {fib4 } представляется: {string.Join(", ", fib5)}");
